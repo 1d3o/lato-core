282db607eb932d37acd11211debd58742a7b8809
Gregorio Galante <gregogalante@gmail.com>
2017-05-09 12:39:55 +0200

    init pagination range functions, add style variable to manage pagination hover background color

--- app/cells/lato_core/elements/pagination/cell.rb
+++ app/cells/lato_core/elements/pagination/cell.rb
@@ -28,6 +28,18 @@ module LatoCore
       def set_conditions
         @prev_disabled_class = @args[:current] > 1 ? '' : 'elements-pagination__arrows--disabled'
         @next_disabled_class = @args[:current] < @args[:total] ? '' : 'elements-pagination__arrows--disabled'
+        @pages_range_init = generate_pages_range_init
+        @pages_range_end = generate_pages_range_end
+      end
+
+      # This function generates the first page number to show on the range.
+      def generate_pages_range_init
+        
+      end
+      
+      # This function generates the last page number to show on the range.
+      def generate_pages_range_end
+
       end
 
   end
