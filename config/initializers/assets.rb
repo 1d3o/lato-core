d77308afd900b6168e98de686064f54647894946
Gregorio Galante <gregogalante@gmail.com>
2017-05-17 16:17:36 +0200

    ntc

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,8 @@
+# Style
+Rails.application.config.assets.precompile += %w( lato_core/application.scss )
+# Javascript
+Rails.application.config.assets.precompile += %w( lato_core/application.js )
+# Images
 Rails.application.config.assets.precompile += %w( lato_core/logo.svg )
 Rails.application.config.assets.precompile += %w( lato_core/logo-negative.svg )
 Rails.application.config.assets.precompile += %w( lato_core/user.png )
