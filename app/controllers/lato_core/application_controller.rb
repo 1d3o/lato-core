ccb840331b2bb5c37c159ffabb6b62267b9c201b
Gregorio Galante <gregogalante@gmail.com>
2017-05-17 09:52:59 +0200

    ntc

--- app/controllers/lato_core/application_controller.rb
+++ app/controllers/lato_core/application_controller.rb
@@ -1,11 +1,15 @@
 module LatoCore
+
+  # ApplicationController
   class ApplicationController < ActionController::Base
+
     protect_from_forgery with: :exception
     skip_before_action :verify_authenticity_token
 
     def index
       redirect_to lato_core.dashboard_path
     end
-    
+
   end
+
 end
--- app/controllers/lato_core/back/authentication_controller.rb
+++ app/controllers/lato_core/back/authentication_controller.rb
@@ -1,39 +1,48 @@
 module LatoCore
-  class Back::AuthenticationController < Back::BackController
+  module Back
 
-    layout 'lato_core/authentication'
+    # AuthenticationController
+    class AuthenticationController < Back::BackController
 
-    skip_before_action :core__manage_superuser_session
-    
-    def index
+      layout 'lato_core/authentication'
 
-    end
+      skip_before_action :core__manage_superuser_session
 
-    def exec_login
-      superuser = LatoCore::Superuser.find_by(username: params[:username])
-      # check superuser exist
-      if !superuser
-        flash[:danger] = LANGUAGES[:lato_core][:flashes][:user_not_found]
-        redirect_to lato_core.login_path
-        return
+      def index; end
+
+      def exec_login
+        @superuser = LatoCore::Superuser.find_by(username: params[:username])
+        return unless check_superuser_exist
+
+        # check superuser is authenticated
+        unless @superuser.authenticate(params[:password])
+          flash[:danger] = LANGUAGES[:lato_core][:flashes][:uncorrect_password]
+          redirect_to lato_core.login_path
+          return
+        end
+
+        # save user login on database and create session
+        superuser.save_login(request.remote_ip)
+        core__create_superuser_session(@superuser, 1.days.from_now)
+        redirect_to lato_core.dashboard_path
       end
-      # check superuser is authenticated
-      if !superuser.authenticate(params[:password])
-        flash[:danger] = LANGUAGES[:lato_core][:flashes][:uncorrect_password]
+
+      def exec_logout
+        core___destroy_superuser_session
+        flash[:success] = LANGUAGES[:lato_core][:flashes][:logout_success]
         redirect_to lato_core.login_path
-        return
       end
-      # save user login on database and create session
-      superuser.save_login(request.remote_ip)
-      core__create_superuser_session(superuser, 1.days.from_now)
-      redirect_to lato_core.dashboard_path
-    end
 
-    def exec_logout
-      core___destroy_superuser_session
-      flash[:success] = LANGUAGES[:lato_core][:flashes][:logout_success]
-      redirect_to lato_core.login_path
-    end
+      private
 
+        def check_superuser_exist
+          unless @superuser
+            flash[:danger] = LANGUAGES[:lato_core][:flashes][:user_not_found]
+            redirect_to lato_core.login_path
+            return false
+          end
+        end
+
+    end
   end
 end
