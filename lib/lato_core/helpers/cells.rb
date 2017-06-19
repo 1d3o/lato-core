c3429b1e45d799bd6f8fa111b3ad0c52a7b4143f
Gregorio Galante <gregogalante@gmail.com>
2017-06-19 11:43:24 +0200

    fix

--- lib/lato_core/helpers/cells.rb
+++ lib/lato_core/helpers/cells.rb
@@ -7,7 +7,7 @@ module LatoCore
     def cell(*names)
       # define variables
       names_list = names.first.to_s.start_with?('lato_') ? names[1..-1] : names
-      cell_class = names.first.to_s.start_with?('lato_') ? "#{names.first.classify}::" : 'LatoCore::'
+      cell_class = names.first.to_s.start_with?('lato_') ? "#{names.first.to_s.classify}::" : 'LatoCore::'
       # return correct cell
       names_list.each do |name|
         cell_class = "#{cell_class}#{name.capitalize}::"
