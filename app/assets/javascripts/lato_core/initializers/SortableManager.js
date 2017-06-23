7853f0d882479d6653b560af99914253399f5cc7
Riccardo Zanutta <riccardo@MBP-di-Riccardo.homenet.telecomitalia.it>
2017-06-23 11:25:30 +0200

    Add sortable hidden inputs, now it works! :)

--- app/assets/javascripts/lato_core/initializers/SortableManager.js
+++ app/assets/javascripts/lato_core/initializers/SortableManager.js
@@ -1,31 +1,48 @@
 var SortableManager = (function () {
   function initSortable() {
-    var sortableContainer = document.querySelector('.js-sortable__main-container')
+    var sortableContainer = document.querySelectorAll('.js-sortable__main-container')
 
-    if (sortableContainer !== null) {
-      var sort = Sortable.create(sortableContainer, {
-        animation: 150,
-        draggable: '.js-sortable__element',
-        onChoose: function (e) {
-          console.info('Element is chosen', e)
-        },
-        onStart: function (e) {
-          console.info('Element dragging started', e)
-        },
-        onEnd: function (e) {
-          console.info('Element dragging ended')
-          console.info(e.oldIndex)
-          console.info(e.newIndex)
-        },
-        onUpdate: function (e) {
-          console.info('onUpdate', e)
-      	},
-        onSort: function (e) {
-          console.info('onSort', e)
-        }
-      })
+    if(sortableContainer.length > 0) {
+      for (var i = 0; i < sortableContainer.length; i++) {
 
-      console.info('sortInstance is: ', sort)
+        var sort = Sortable.create(sortableContainer[i], {
+          animation: 150,
+          draggable: '.js-sortable__element',
+          onChoose: function (e) {
+            console.info('Element is chosen', e)
+          },
+          onStart: function (e) {
+            console.info('Element dragging started', e)
+          },
+          onEnd: function (e) {
+            console.info('Element dragging ended')
+            console.info(e.oldIndex)
+            console.info(e.newIndex)
+
+            if (e.oldIndex !== e.newIndex) {
+              updateOrder(this.el)
+            }
+          },
+          onUpdate: function (e) {
+            console.info('onUpdate', e)
+        	},
+          onSort: function (e) {
+            console.info('onSort', e)
+          }
+        })
+      }
+    }
+  }
+
+  function updateOrder(container) {
+    var sortableElements = container.querySelectorAll('.js-sortable__element')
+    for (var i = 0; i < sortableElements.length; i++) {
+      var sortableElement = sortableElements[i]
+      var sortableElementId = sortableElement.dataset.id
+      var relativeInput = document.querySelector('.js-sortable__hidden-input[data-id="' + sortableElementId + '"]')
+
+      relativeInput.value = i + 1
+      sortableElement.dataset.position = i + 1
     }
   }
 
