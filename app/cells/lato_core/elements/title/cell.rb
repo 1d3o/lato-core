4af0b13da4a97ac79f29abb32d19d70c7a144cdc
Gregorio Galante <gregogalante@gmail.com>
2017-08-12 08:26:47 +0200

    add icon on titles

--- app/cells/lato_core/elements/title/cell.rb
+++ app/cells/lato_core/elements/title/cell.rb
@@ -5,7 +5,8 @@ module LatoCore
     @@requested_args = [:label]
 
     @@default_args = {
-      size: 1
+      size: 1,
+      icon: nil
     }
 
     def initialize(args = {})
@@ -24,28 +25,29 @@ module LatoCore
 
     private
 
-      def set_conditions
-        @html_tag = get_html_tag
+    def set_conditions
+      @html_tag = get_html_tag
+      @show_icon = @args[:icon] ? true : false
+    end
+
+    def get_html_tag
+      case @args[:size]
+      when 1
+        return 'h1'
+      when 2
+        return 'h2'
+      when 3
+        return 'h3'
+      when 4
+        return 'h4'
+      when 5
+        return 'h5'
+      when 6
+        return 'h6'
+      else
+        return 'h1'
       end
-      
-      def get_html_tag
-        case @args[:size]
-        when 1
-          return 'h1'
-        when 2
-          return 'h2'
-        when 3
-          return 'h3'
-        when 4
-          return 'h4'
-        when 5
-          return 'h5'
-        when 6
-          return 'h6'
-        else
-          return 'h1'
-        end
-      end
+    end
 
   end
 
